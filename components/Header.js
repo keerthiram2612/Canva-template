4c93089daeca170f2d1e753f058019e2cf6a13bb
keerthiram2612 <keerthiram2612@gmail.com>
2023-02-03 09:58:52 +0530

    fixing the canva logo in header section

--- components/Header.js
+++ components/Header.js
@@ -4,16 +4,20 @@ import User from './user';
 export default function Header(){
     return(
         <header className="flex justify-between p-5 text-sm">
-            <div className="flex space-x-4 items-center">
-                <p className="link">Design spotlight</p>
-                <p className="link">Bussiness</p>
-                <p className="link">Education</p>
-                <p className="link">plans and pricing</p>
-                <p className="link">Learn</p>
+            <div className="flex space-x-8 items-center  hover:underline">
+                <img className ="w-20 ml-10 object-cover cursor-pointer"src="https://static-cse.canva.com/_next/static/assets/logo_w2000xh641_3b021976d60d0277e95febf805ad9fe8c7d6d54f86969ec03b83299084b7cb93.png" 
+                alt="canva-logo"/>
+                <p className="link  text-gray-600 ">Design spotlight</p>
+                <p className="link  text-gray-600">Bussiness</p>
+                <p className="link text-gray-600">Education</p>
+                <p className="link  text-gray-600">plans and pricing</p>
+                <p className="link  text-gray-600">Learn</p>
+              
             </div>
-            <div className="flex space-x-4 items-center">
-                <p className="link">Log in</p>
-                <p className="link">Sign up</p>
+            <div className="flex space-x-4 items-center hover:underline">
+                <p className="link  text-gray-600">Log in</p>
+                <p className="link  text-gray-600">Sign up</p>
+                 <User/>
                 
             </div>
         </header>
--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -7,6 +7,14 @@ return(
     <>
      <Header/>
      <div className="">
+        {Object.values(providers).map(provider=>(
+            <div key={provider.name} className="">
+                <img src="https://static-cse.canva.com/_next/static/assets/logo_w2000xh641_3b021976d60d0277e95febf805ad9fe8c7d6d54f86969ec03b83299084b7cb93.png" 
+                alt="canva-logo"/>
+                <p>This website is created for styling purposes</p>
+                <button>Sign in with {provider.name}</button>
+                </div>
+        ))}
         
         
      </div>
